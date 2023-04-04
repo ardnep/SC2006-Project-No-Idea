8fef8528f46ac28bccae4a3678cdb173152ed6aa
taica00 <64315786+taica00@users.noreply.github.com>
2023-04-05 04:47:33 +0800

    executed trips in list and grouped by date

--- MyTripDiary/src/controllers/HistoryController.js
+++ MyTripDiary/src/controllers/HistoryController.js
@@ -1,14 +1,36 @@
+import { getAllExecutedTrips } from "./SavedTripsController";
+
 /**
  * Get all the executed trips
  * @returns {Array} all executed trips for this user 
  */
-function getAllExecutedTrips() { }
+export function getAllExecutedTripsList() { 
+    const executedTripsMap = getAllExecutedTrips();
+    return [...executedTripsMap.values()].flat();
+}
+
+function getDate(timestamp) {
+    let date = new Date(1970, 0, 1);
+    date.setSeconds(timestamp);
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
 
 /**
  * Get sorted list of executed trips
  * @returns {Array} all executed trips sorted
  */
-function getSortedList() { }
+export function getExecutedTripsGroupedByDate() { 
+    let executedTrips = getAllExecutedTripsList();
+    const groupedTrips = executedTrips.reduce((acc, curr) => {
+        const date = getDate(curr.timeStamp.seconds);
+        if (!acc[date]) {
+          acc[date] = [];
+        }
+        acc[date].push(curr);
+        return acc;
+    }, {});
+    return groupedTrips;
+}
 
 /**
  * Get filtered list of executed trips based on filters
