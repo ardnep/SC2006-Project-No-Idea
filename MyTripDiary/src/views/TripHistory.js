d18dd6185ca1eb1a5e28f1dcee06ac2337cf9b86
taica00 <taichenan00@gmail.com>
2023-04-06 18:14:03 +0800

    wip

--- MyTripDiary/src/views/TripHistory.js
+++ MyTripDiary/src/views/TripHistory.js
@@ -1,39 +1,90 @@
 import { Ionicons } from '@expo/vector-icons';
 import React, { useState } from 'react';
-import {   TouchableHighlight, SectionList, StyleSheet, StatusBar, View, FlatList } from 'react-native';
-import { Layout, Text, TopNav, useTheme } from 'react-native-rapi-ui';
-import { getExecutedTripsGroupedByDate } from '../controllers/HistoryController';
+import { SectionList, StyleSheet, StatusBar, View, Pressable } from 'react-native';
+import { Layout, Section, SectionContent, Text, TopNav, useTheme } from 'react-native-rapi-ui';
+import { getExecutedTripsSortedByDate } from '../controllers/HistoryController';
 
 /** Displays TripHistory screen */
 export default function ({ navigation }) {
     const { isDarkmode } = useTheme();
-    const [executedTrips, setExecutedTrips] = useState(getExecutedTripsGroupedByDate());
-    console.log(getExecutedTripsGroupedByDate());
-    const renderExecutedTrip = ({ item }) => {
-        const trip = item;
+    const [executedTrips, setExecutedTrips] = useState(getExecutedTripsSortedByDate());
+    let groupedTrips = groupExecutedTripsByDate(executedTrips);
+
+    const renderSectionHeader = ({ section }) => {
         return (
-            <Pressable onPress={() => { navigation.navigate("SavedTripInfo", { trip, updateSavedTrips }) }}>
-                <Section style={styles.section}>
-                    <Text>{trip.name}</Text>
-                    <SectionContent>
-                        <Text>{trip.srcName} to {trip.destName}</Text>
+            <Section style={styles.sectionHeaderContainer}>
+                <Text size='xl' fontWeight="medium">{section.title}</Text>
+            </Section>
+        );
+    };
+
+    const renderTrip = ({ item }) => {
+        const timestamp = item.timeStamp.seconds;
+        const startTime = getTime(timestamp);
+        const endTime = getTime(timestamp + item.duration*60)
+        return (
+            <Pressable onPress={() => { navigation.navigate() }}>
+                <Section style={styles.itemContainer}>
+                    <SectionContent style={styles.timeAndPriceContainer}>
+                        <Text>{startTime} - {endTime}</Text>
+                        <Text>${item.tripPrice.toFixed(2)}</Text>
                     </SectionContent>
+                    <Text>Mode of transport: {item.modeOfTransport}</Text>
                 </Section>
             </Pressable>
         )
     }
 
     return (
-		<Layout>
+        <Layout>
             <TopNav
                 leftContent={<Ionicons name="chevron-back" color={isDarkmode ? 'white' : 'black'} size={20} />}
                 leftAction={navigation.goBack}
                 middleContent="Trip History"
             />
-		</Layout>
-	);
+            <SectionList
+                sections={groupedTrips}
+                renderItem={renderTrip}
+                renderSectionHeader={renderSectionHeader}
+            />
+        </Layout>
+    );
 }
 
+function groupExecutedTripsByDate(executedTrips) {
+    const groupedTrips = executedTrips.reduce((result, item) => {
+        const date = getDate(item.timeStamp.seconds);
+        if (!result[date]) {
+            result[date] = [];
+        }
+        result[date].push(item);
+        return result;
+    }, {});
+    const sections = Object.keys(groupedTrips).map((date) => ({
+        title: date,
+        data: groupedTrips[date],
+    }));
+    return sections;
+}
+
+function getDateObject(timestamp) {
+    let date = new Date(1970, 0, 1);
+    date.setSeconds(timestamp);
+    return date;
+}
+
+function getDate(timestamp) {
+    const date = getDateObject(timestamp);
+    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+}
+
+function getTime(timestamp) {
+    const date = getDateObject(timestamp);
+    let minutes = date.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+    return date.getHours() + ":" + minutes;
+}
 
 /** 
  * OneTrip component. Displays information for each executed trip, and contains
@@ -45,37 +96,36 @@ export default function ({ navigation }) {
 // ]}
 const OneTrip = props => {
     return (
-      <View style={styles.itemContainer}>
-        <View style={styles.timeAndPriceContainer}>
-            <Text>{props.startTime} - {props.endTime}</Text>
-            <Text>${props.price}</Text>
+        <View style={styles.itemContainer}>
+            <View style={styles.timeAndPriceContainer}>
+                <Text>{props.startTime} - {props.endTime}</Text>
+                <Text>${props.price}</Text>
+            </View>
+            <View style={styles.startAndEndPointContainer}>
+                <Text>{props.startPoint} to {props.endPoint}</Text>
+            </View>
         </View>
-        <View style={styles.startAndEndPointContainer}>
-            <Text>{props.startPoint} to {props.endPoint}</Text>
-        </View>
-      </View>
     );
 };
 
 /** Defines styles used in the screen. */
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      marginTop: StatusBar.currentHeight || 0,
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
     },
-    sectionHeader: {
+    sectionHeaderContainer: {
         paddingLeft: 16,
-        fontSize: 20,
-        backgroundColor: 'rgba(247,247,247,1.0)',
+        paddingVertical: 4,
     },
     item: {
-        backgroundColor: '#00fb9a',
-        padding: 16,  
+        padding: 16,
         marginVertical: 8,
         marginHorizontal: 16,
     },
     itemContainer: {
-        flex: 1,
+        elevation: 16,
+        margin: 8  
     },
     timeAndPriceContainer: {
         flex: 1,
