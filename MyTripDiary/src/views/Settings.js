0fec040fca4845a36587f4109995fc9461fc682a
ardnep <aranepal@gmail.com>
2023-04-11 23:35:00 +0800

    settings email and name change works

--- MyTripDiary/src/views/Settings.js
+++ MyTripDiary/src/views/Settings.js
@@ -1,68 +1,61 @@
-import { StyleSheet, Text, SafeAreaView, Button, TouchableOpacity, Image } from 'react-native';
-import { firebaseAuth } from '../controllers/FirebaseController';
+import React from 'react';
+
+import { StyleSheet, TouchableOpacity, Image, View } from 'react-native';
+import { firebaseAuth, getUserDisplayName } from '../controllers/FirebaseController';
+import { Layout, TopNav, Avatar, Text } from 'react-native-rapi-ui';
+
+import { useTheme } from 'react-native-rapi-ui';
 
 /**
  * Displays the Settings app onto the screen 
  */
 export default function Settings({ navigation }) {
-  const currentUser = firebaseAuth.currentUser;
+  const { isDarkmode, setTheme } = useTheme();
+
+  const [displayName, setDisplayName] = React.useState(getUserDisplayName());
+
+  const updateDisplayName = () => {
+    setDisplayName(getUserDisplayName());
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Settings</Text>
+    <Layout>
+      <TopNav
+        middleContent="Settings"
+
+      />
       <Image
         style={styles.icon}
-        source={currentUser.photoURL ? currentUser.photoURL : '../../assets/profile-user.png'} />
-      <TouchableOpacity onPress={() => { navigation.navigate("AccountSettings", { currentUser }) }}>
-        <Text style={styles.optiontext}>Account</Text>
+        source={require('../../assets/profile-user.png')} />
+      <View style={{ height: "10%", justifyContent: 'space-between', alignItems: 'center', marginTop: 20 }}>
+        <Text style={styles.text}>{displayName || 'N/A'}</Text>
+      </View>
+      <TouchableOpacity onPress={() => { navigation.navigate("AccountSettings", { updateDisplayName }) }}>
+        <Text style={styles.optionText}>Account</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => { navigation.navigate("PersonalizationSettings") }}>
-        <Text style={styles.optiontext}>Personalisation</Text>
+      <TouchableOpacity onPress={() => { setTheme(isDarkmode ? "light" : "dark") }}>
+        <Text style={styles.optionText}>{isDarkmode ? "Switch to Light Mode" : "Switch to Dark Mode"}</Text>
       </TouchableOpacity>
       <TouchableOpacity>
-        <Text style={styles.optiontext}>Help</Text>
+        <Text style={styles.optionText}>Help</Text>
       </TouchableOpacity>
       <Text style={styles.ver}>Ver 1.0</Text>
-    </SafeAreaView>
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
-  title: {
-    marginTop: 0,
-    marginLeft: 0,
-    paddingVertical: 0,
-    color: '#20232a',
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-  },
   icon: {
     alignSelf: 'center',
     marginTop: 30,
     width: 150,
     height: 150,
   },
-  optiontextf: {
-    marginTop: 30,
+  optionText: {
     padding: 10,
     marginBottom: 20,
     fontSize: 20,
     fontWeight: 'light',
-    borderBottomColor: 'grey',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optiontext: {
-    padding: 10,
-    marginBottom: 20,
-    fontSize: 20,
-    fontWeight: 'light',
-    borderBottomColor: 'grey',
     borderBottomWidth: StyleSheet.hairlineWidth,
   },
   ver: {
